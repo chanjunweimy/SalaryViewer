ee6c2ffe672a0429903b912701dc6a2906c24d28
chanjunweimy <chanjunweimy@gmail.com>
2019-06-26 22:46:56 +0800

    just use simple mutually exclusive lock

--- src/main/java/cdit/service/UserServiceImpl.java
+++ src/main/java/cdit/service/UserServiceImpl.java
@@ -1,8 +1,7 @@
 package cdit.service;
 
 import java.util.List;
-import java.util.concurrent.locks.ReadWriteLock;
-import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.concurrent.locks.ReentrantLock;
 import org.springframework.stereotype.Service;
 import cdit.dao.UserRepository;
 import cdit.model.User;
@@ -10,33 +9,32 @@ import cdit.model.User;
 @Service()
 public class UserServiceImpl implements UserService {
   private final UserRepository _userRepository;
-  private ReadWriteLock _lock;
+  private final ReentrantLock _lock = new ReentrantLock();
   
   public UserServiceImpl(UserRepository userRepository) {
     _userRepository = userRepository;
-    _lock = new ReentrantReadWriteLock();
   }
 
   @Override
   public void updateUsers(List<User> users) {
-    _lock.writeLock().lock();
+    _lock.lock();
     try {
       _userRepository.deleteAll();
       _userRepository.saveAll(users);  
       _userRepository.flush();
     } finally {
-      _lock.writeLock().unlock();
+      _lock.unlock();
     }    
   }
 
   @Override
   public List<User> getAllUsers() {
-    _lock.readLock().lock();
+    _lock.lock();
     List<User> users = null;
     try {
       users = _userRepository.findAll();
     } finally {
-      _lock.readLock().unlock();
+      _lock.unlock();
     }    
     return users;
   }
