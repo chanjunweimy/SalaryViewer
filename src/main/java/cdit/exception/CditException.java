e3cf509b783abb77a1e2dd8ba21498ee3067b39f
chanjunweimy <chanjunweimy@gmail.com>
2019-06-30 13:43:25 +0800

    Fixed property issues

--- src/main/java/cdit/exception/CditException.java
+++ src/main/java/cdit/exception/CditException.java
@@ -1,6 +1,11 @@
 package cdit.exception;
 
 public class CditException extends Exception {
+  /**
+   * 
+   */
+  private static final long serialVersionUID = 8318928365485639988L;
+  
   public CditException() {
     super();
   }
@@ -8,9 +13,4 @@ public class CditException extends Exception {
   public CditException(String message) {
     super(message);
   }
-
-  /**
-   * 
-   */
-  private static final long serialVersionUID = 8318928365485639988L;
 }
--- src/main/java/cdit/exception/InvalidCsvException.java
+++ src/main/java/cdit/exception/InvalidCsvException.java
@@ -1,17 +1,16 @@
 package cdit.exception;
 
 public class InvalidCsvException extends CditException {
-
+  /**
+   * 
+   */
+  private static final long serialVersionUID = -6269600736150550219L;
+  
   public InvalidCsvException() {
     super();
   }
 
   public InvalidCsvException(String message) {
     super(message);
-  }
-
-  /**
-   * 
-   */
-  private static final long serialVersionUID = -6269600736150550219L;
+  } 
 }
